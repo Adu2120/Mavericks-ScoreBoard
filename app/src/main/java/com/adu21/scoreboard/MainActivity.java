cf14d03db60e4a98af59324e1419052cdce2d09e
Aditya Kavale <adityakavale21@gmail.com>
2021-02-22 20:54:57 +0530

    2nd commit

--- app/src/main/java/com/adu21/scoreboard/MainActivity.java
+++ app/src/main/java/com/adu21/scoreboard/MainActivity.java
@@ -1,7 +1,5 @@
 package com.adu21.scoreboard;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -11,11 +9,14 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     int i = 0;
     String tableRows;
     private EditText et_student;
     private TableLayout student_table;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
         tableRows = "Tablerow" + i;
         i++;
         //Trying to add more Parameters
-        TableRow.LayoutParams params = new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.MATCH_PARENT);
+        TableRow.LayoutParams params = new TableRow.LayoutParams(
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                TableLayout.LayoutParams.MATCH_PARENT
+        );
 
         TableRow tableRow = new TableRow(this);
         if (!et_student.getText().toString().equals("")) {
@@ -37,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
             txt.setText(et_student.getText().toString());
             txt.setTextSize(18);
             txt.setLayoutParams(params);
-            txt.setPadding(0, 0, 150, 0);
+            txt.setPadding(0, 0, 130, 0);
             txt.setGravity(Gravity.CENTER);
 
             TextView cnt = new TextView(this);
@@ -45,13 +49,11 @@ public class MainActivity extends AppCompatActivity {
             cnt.setTextSize(18);
             cnt.setLayoutParams(params);
             cnt.setPadding(0, 0, 30, 0);
-            cnt.setId(i);
             cnt.setGravity(Gravity.CENTER);
 
             ImageButton add = new ImageButton(this);
             add.setImageResource(R.drawable.ic_counter);
             add.setLayoutParams(params);
-            add.setId(i);
             add.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -61,9 +63,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+            ImageButton sub = new ImageButton(this);
+            sub.setImageResource(R.drawable.ic_dec);
+            sub.setLayoutParams(params);
+            sub.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int count = Integer.parseInt(cnt.getText().toString());
+                    count--;
+                    cnt.setText(Integer.toString(count));
+                }
+            });
+
             tableRow.addView(txt);
             tableRow.addView(cnt);
             tableRow.addView(add);
+            tableRow.addView(sub);
             student_table.addView(tableRow);
             et_student.setText("");
         }
